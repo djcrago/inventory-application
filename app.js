54e571bdd0e55baea299532a2c462b5062555311
Dexter Crago <jodieanddexter@gmail.com>
2024-05-01 16:38:05 -0600

    Install and use deployment dependencies in app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 // Require routers
 const indexRouter = require('./routes/index');
@@ -19,6 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 
 // Create mongoose connection to database
 const mongoose = require('mongoose');
