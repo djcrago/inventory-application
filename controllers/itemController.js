cd99bfc199d6ad2795c152f77d6c762e3648e932
Dexter Crago <jodieanddexter@gmail.com>
2024-04-30 20:12:13 -0600

    Implement item_detail view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -39,14 +39,18 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all Item.
 exports.item_list = asyncHandler(async (req, res, next) => {
-  const allItems = await Item.find().sort({ name: 1 }).exec();
+  const allItems = await Item.find({}, 'name price number_in_stock')
+    .sort({ name: 1 })
+    .exec();
 
   res.render('item_list', { title: 'List of all Items', item_list: allItems });
 });
 
 // Display detail page for a specific Item.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item Detail');
+  const item = await Item.findById(req.params.id).populate('category').exec();
+
+  res.render('item_detail', { item });
 });
 
 // Display Item create form on GET.
